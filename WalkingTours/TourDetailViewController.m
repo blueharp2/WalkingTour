0408e3dc60552165df2a419db82d3182a3dcb917
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-12-14 14:18:11 -0800

    Start of TourDetailViewController

---
+++ WalkingTours/TourDetailViewController.m
@@ -0,0 +1,35 @@
+//
+//  TourDetailViewController.m
+//  WalkingTours
+//
+//  Created by Lindsey on 12/14/15.
+//  Copyright © 2015 Lindsey Boggio. All rights reserved.
+//
+
+#import "TourDetailViewController.h"
+#import "Location.h"
+
+@interface TourDetailViewController ()
+
+
+@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+@property (weak, nonatomic) IBOutlet UILabel *locationDescriptionLabel;
+
+@end
+
+
+
+@implementation TourDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+@end
