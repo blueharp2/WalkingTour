be2e046bc22fe0c4cf95a26da66560a08e4f434a
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-12-14 15:13:52 -0800

    Parse Code

--- ParseLoginViewController.m
+++ ParseLoginViewController.m
@@ -6,28 +6,23 @@
 //  Copyright © 2015 Lindsey Boggio. All rights reserved.
 //
 
+@import UIKit;
+@import Parse;
 #import "ParseLoginViewController.h"
-#import "Parse/Parse.h"
-#import <ParseUI/ParseUI.h>
-
+#import "ParseUI/ParseUI.h"
 
 @interface ParseLoginViewController () <UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *userNameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
-- (IBAction)loginButton:(id)sender;
-
 @end
 
 @implementation ParseLoginViewController
 
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self signUp];
-    [self login];
+
 }
 
 - (IBAction)loginButton:(id)sender {
@@ -36,92 +31,37 @@
     NSString *password = _passwordField.text;
     
     PFUser *user = [PFUser user];
-        user.username = @"my name";
-        user.password = @"my pass";
-        user.email = @"email@example.com";
-        
-        // other fields can be set just like with PFObject
-        user[@"phone"] = @"415-392-0202";
-        
+        user.username = username;
+        user.password = password;
+    
         [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-            if (!error) {   // Hooray! Let them use the app now.
-            } else {   NSString *errorString = [error userInfo][@"error"];   // Show the errorString somewhere and let the user try again.
+            if (!error) {
+                } else {
+                    NSString *errorString = [error userInfo][@"error"];
             }
         }];
-    }
     
     [PFUser logInWithUsernameInBackground:@"username" password:@"password"
                                     block:^(PFUser *user, NSError *error) {
-                                        if (user) {
-                                            // Do stuff after successful login.
-                                        } else {
-                                            // The login failed. Check error to see why.
-                                        }
-                                    }];
-    
-    PFUser *currentUser = [PFUser currentUser];
-    if (currentUser) {
-        // do stuff with the user
-    } else {
-        // show the signup or login screen
-    }
-
-    [PFUser logOut];
-    PFUser *currentUser = [PFUser currentUser]; // this will now be nil
-
-    }
+            if (user != nil) {
+                // Do stuff after successful login.
+            } else {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"My Alert" message:@"This is an alert." preferredStyle:UIAlertControllerStyleAlert]; // 7
+        
+        UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+            NSLog(@"You pressed button OK");
+        }];
+        
+        [alert addAction:defaultAction];
+        
+        [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+            textField.placeholder = @"Input data...";
+        }];
+        
+        [self presentViewController:alert animated:YES completion:nil];
+                
+        }
+    }];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-}
-
-//let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
-//let action = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
-//myAlert.addAction(action)
-//
-//self.presentViewController(myAlert, animated: true, completion:nil)
-
-
 @end
 
-//    var userEmail = userEmailAddressTextField.text!
-//    var userPassword = userPasswordTextField.text!
-//    
-//    if (userEmail.isEmpty || userPassword.isEmpty) {
-//        return
-//    }
-//    
-//    PFUser.logInWithUsernameInBackground(userEmail, password: userPassword) { (user, error) -> Void in
-//        
-//        var userMessage = "Welcome!"
-//        
-//        if(user != nil) {
-//            
-//            let userName:String? = user?.username
-//            
-//            NSUserDefaults.standardUserDefaults().setObject(userName, forKey: "user_name")
-//            NSUserDefaults.standardUserDefaults().synchronize()
-//            
-//            let mainStoryboard = UIStoryboard(name:"Main", bundle:nil)
-//            
-//            let mainPage:MainPageViewController = mainStoryboard.instantiateViewControllerWithIdentifier("MainPageViewController") as! MainPageViewController
-//            
-//            let mainPageNav = UINavigationController(rootViewController: mainPage)
-//            
-//            let appDelegate:AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-//            
-//            appDelegate.window?.rootViewController = mainPageNav
-//            
-//        } else {
-//            
-//            userMessage = error!.localizedDescription
-//        }
-//        
-//        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
-//        let action = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
-//        myAlert.addAction(action)
-//        
-//        self.presentViewController(myAlert, animated: true, completion:nil)
-//    }
-//}
