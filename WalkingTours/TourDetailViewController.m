85c957516ae57789ff957007a8950c64a62f2d5f
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-12-14 15:08:19 -0800

    TourDetailViewController Updated and start of CreateTourTableViewController

--- WalkingTours/TourDetailViewController.m
+++ WalkingTours/TourDetailViewController.m
@@ -12,8 +12,6 @@
 @interface TourDetailViewController ()
 
 
-@property (weak, nonatomic) IBOutlet UIImageView *imgView;
-@property (weak, nonatomic) IBOutlet UILabel *locationDescriptionLabel;
 
 @end
 
