a89c49ea1c7aaa3384906e54096f063ed2312c42
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-12-15 09:50:00 -0800

    Merge

--- CreateTourDetailViewController.m
+++ CreateTourDetailViewController.m
@@ -69,7 +69,7 @@
         mediaData = UIImageJPEGRepresentation([info objectForKey:@""], 1.0);
         mediaFile = [PFFile fileWithName:[NSString stringWithFormat:@"%i.jpg",rand() / 2] data:mediaData];
     }
-    [self addTestModelsToParse:mediaFile];
+//    [self addTestModelsToParse:mediaFile];
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -77,21 +77,21 @@
     [self loadImagePicker];
 }
 
-- (void)addTestModelsToParse:(PFFile *)media {
-    CLLocationCoordinate2D location = CLLocationCoordinate2DMake(47.623544, -122.336224);
-    PFGeoPoint *geoPoint = [PFGeoPoint geoPointWithLatitude:location.latitude longitude:location.longitude];
-    Tour *tour1 = [[Tour alloc] initWithClassName:@"Tour" nameOfTour:@"Tour 2." descriptionText:@"This is the tour description" startLocation:geoPoint user:nil];
-    Location *location1 = [[Location alloc] initWithClassName:@"Location" locationName:@"Code Fellows" locationDescription:@"This is where we practically live" photo:media categories:@[@"School", @"Education"] location:geoPoint tour:tour1];
-    PFGeoPoint *geoPoint2 = [PFGeoPoint geoPointWithLatitude:47.627825 longitude:-122.337412];
-    Location *location2 = [[Location alloc] initWithClassName:@"Location" locationName:@"The Park" locationDescription:@"I remember what the sun was like..." photo:media categories:@[@"Park", @"Not Coding"] location:geoPoint2 tour:tour1];
-    NSArray *objectArray = [NSArray arrayWithObjects:tour1, location1, location2, nil];
-    [PFObject saveAllInBackground:objectArray block:^(BOOL succeeded, NSError * _Nullable error) {
-        if (succeeded) {
-            NSLog(@"They saved!");
-        } else {
-            NSLog(@"Something went terribly wrong.");
-        }
-    }];
-}
+//- (void)addTestModelsToParse:(PFFile *)media {
+//    CLLocationCoordinate2D location = CLLocationCoordinate2DMake(47.623544, -122.336224);
+//    PFGeoPoint *geoPoint = [PFGeoPoint geoPointWithLatitude:location.latitude longitude:location.longitude];
+//    Tour *tour1 = [[Tour alloc] initWithClassName:@"Tour" nameOfTour:@"Tour 2." descriptionText:@"This is the tour description" startLocation:geoPoint user:nil];
+//    Location *location1 = [[Location alloc] initWithClassName:@"Location" locationName:@"Code Fellows" locationDescription:@"This is where we practically live" photo:media categories:@[@"School", @"Education"] location:geoPoint tour:tour1];
+//    PFGeoPoint *geoPoint2 = [PFGeoPoint geoPointWithLatitude:47.627825 longitude:-122.337412];
+//    Location *location2 = [[Location alloc] initWithClassName:@"Location" locationName:@"The Park" locationDescription:@"I remember what the sun was like..." photo:media categories:@[@"Park", @"Not Coding"] location:geoPoint2 tour:tour1];
+//    NSArray *objectArray = [NSArray arrayWithObjects:tour1, location1, location2, nil];
+//    [PFObject saveAllInBackground:objectArray block:^(BOOL succeeded, NSError * _Nullable error) {
+//        if (succeeded) {
+//            NSLog(@"They saved!");
+//        } else {
+//            NSLog(@"Something went terribly wrong.");
+//        }
+//    }];
+//}
 
 @end
