03071d5c925263d789f804a52e30a332b3f39f37
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2015-12-14 18:51:57 -0800

    CreatTourViewController work continued

--- WalkingTours/CreateTourViewController.m
+++ WalkingTours/CreateTourViewController.m
@@ -10,31 +10,44 @@
 
 
 
-@interface CreateTourViewController()
+@interface CreateTourViewController() <UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *nameOfTourTextField;
-
 @property (weak, nonatomic) IBOutlet UITextField *tourDescriptionTextField;
 
 @property (weak, nonatomic) IBOutlet UIButton *addLocationButton;
-
 - (IBAction)addLocationsButton:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UITableView *locationTableView;
 
 
 @end
 
 @implementation CreateTourViewController
 
-
+-(void) setLocationTableView:(UITableView *)locationTableView{
+    [self.locationTableView reloadData];
+}
 
 
 -(void)viewDidLoad{
     [super viewDidLoad];
+    [self setupMainViewController];
     
     UIBarButtonItem *saveButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(saveTour:)];
     self.navigationItem.rightBarButtonItem = saveButton;
 }
 
+
 - (IBAction)addLocationsButton:(id)sender {
 }
+
+-(void)setupMainViewController{
+    [self.locationTableView setDelegate:self];
+    [self.locationTableView setDataSource:self];
+    
+}
+
+
+
 @end
