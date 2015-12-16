5cd3fa710b0d338680f5fe23f9b36a280fe7645b
Miles Ranisavljevic <itstop3@gmail.com>
2015-12-15 18:41:16 -0800

    Update ParseService fetch functions.

--- ParseService.h
+++ ParseService.h
@@ -9,9 +9,13 @@
 #import <Foundation/Foundation.h>
 #import "Tour.h"
 
+typedef void(^locationsFetchCompletion)(BOOL success, NSArray *results);
+typedef void(^toursFetchCompletion)(BOOL success, NSArray *results);
 
 @interface ParseService : NSObject
 
 + (void)saveToParse:(Tour *)tour locations:(NSArray *)locations;
++ (void)fetchLocationsWithTourId:(NSString *)tourId completion:(locationsFetchCompletion)completion;
++ (void)fetchToursNearLocation:(CLLocationCoordinate2D)location completion:(toursFetchCompletion)completion;
 
 @end
