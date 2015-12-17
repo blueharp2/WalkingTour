cc4b069df798bc64a4001181430d9335711403da
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-12-17 15:35:23 -0800

    Rework

--- ParseResetPasswordViewController.m
+++ ParseResetPasswordViewController.m
@@ -31,27 +31,6 @@
 
 }
 
-        //Test for Valid Email Address
-
-//- (BOOL) validateEmail: (UITextField *)passwordReset {
-//    NSString *emailString = self.passwordReset.text;
-//    NSString *emailRegex = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
-//    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
-//    
-////    return [emailTest evaluateWithObject:passwordReset];
-//
-//    if (([emailTest evaluateWithObject:emailString] != YES) || [emailString isEqualToString:@""])
-//    {
-//        UIAlertController *loginalert = [UIAlertController alertControllerWithTitle:@"Email Invalid" message:@"Enter Email in abc@example.com Format" preferredStyle:UIAlertControllerStyleAlert;
-//        
-//        
-//        [loginalert show];
-//        return NO;
-//    } else {
-//        return YES;
-////    If email is invalid, it will remind the user with an alert box. 
-//    }
-//}
 
 - (IBAction)cancelPressed:(UIBarButtonItem *)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
