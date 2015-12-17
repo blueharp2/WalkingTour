dbd1065d43fa38363e99cb26a5063a15c94319a9
Alberto-Vega <el_vega@live.com>
2015-12-17 09:21:16 -0800

    Fixed gradient.

---
+++ Gradient.h
@@ -0,0 +1,15 @@
+//
+//  Gradient.h
+//  WalkingTours
+//
+//  Created by Alberto Vega Gonzalez on 12/16/15.
+//  Copyright © 2015 Lindsey Boggio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import QuartzCore;
+
+@interface Gradient : NSObject
+
++(CAGradientLayer*) blueGradient;
+@end
