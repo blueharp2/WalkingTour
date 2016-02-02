871a08aa8b9987dfaeab8e2a976e1cc515560a53
LINDSEY BOGGIO <BLUEHARP2@GMAIL.COM>
2016-02-02 12:10:29 -0800

    tour objectId can be added to the favorite tours array

--- FindToursViewController.m
+++ FindToursViewController.m
@@ -495,9 +495,15 @@
     
     if (indexPath != nil) {
         NSMutableArray *favoriteToursFromParse = [[NSMutableArray alloc]init];
-        [favoriteToursFromParse addObject: @"objectId"];
-      //  [favoriteToursFromParse valueForKeyPath:@"objectId"];
-//        [favoriteToursFromParse addObject:indexPath];
+        Tour *tour = self.toursFromParse[indexPath.section];
+        NSString *tourObjectId = tour.objectId;
+        [favoriteToursFromParse addObject:tour.objectId];
+        
+        //[favoriteToursFromParse addObject:self.toursFromParse[indexPath.section]];//crashes
+        //[favoriteToursFromParse addObject:tour.objectId];//can't find tour
+        //[favoriteToursFromParse addObject: @"objectId"];//logs the string "objectId"
+      //  [favoriteToursFromParse valueForKeyPath:@"objectId"];//doesn't add anything to the array
+//        [favoriteToursFromParse addObject:indexPath];//crashes
         NSString *test = [favoriteToursFromParse objectAtIndex:0];
         NSLog(test);
     }
