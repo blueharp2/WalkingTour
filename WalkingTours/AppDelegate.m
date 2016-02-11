530cd630e19e2a8e73fda67db8eab505aba6c352
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2016-02-10 17:05:36 -0800

    urlScheme walkabouttours created -

--- WalkingTours/AppDelegate.m
+++ WalkingTours/AppDelegate.m
@@ -30,8 +30,74 @@
     NSString *stringUrl = [NSString stringWithFormat:@"%@", url];
     if ([stringUrl containsString:@"walkabouttours://"]) {
         //Do something with the url
+        
+        
+//        if (!url) {  return NO; }
+//        
+//        NSString *URLString = [url absoluteString];
+//        [[NSUserDefaults standardUserDefaults] setObject:URLString forKey:@"url"];
+//        [[NSUserDefaults standardUserDefaults] synchronize];
+//        return YES;
+//    }
+        NSLog(@"url recieved: %@", url);
+        NSLog(@"query string: %@", [url query]);
+        NSLog(@"host: %@", [url host]);
+        NSLog(@"url path: %@", [url path]);
+        NSDictionary *dict = [self parseQueryString:[url query]];
+        NSLog(@"query dict: %@", dict);
+        return YES;
+        
     }
     return NO;
 }
 
+//method for parsing the query string from the url
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    
+    if ([[url scheme] isEqualToString:@"id="]) {
+        NSString *query = [url query];
+        if (query.length > 0) {
+            NSArray *components = [query componentsSeparatedByString:@"&"];
+            NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+            for (NSString *component in components) {
+                NSArray *subcomponents = [component componentsSeparatedByString:@"="];
+                [parameters setObject:[[subcomponents objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]
+                               forKey:[[subcomponents objectAtIndex:0] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+            }
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    
+}
+
+- (NSDictionary *)parseQueryString:(NSString *)query {
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithCapacity:6];
+    NSArray *pairs = [query componentsSeparatedByString:@"&"];
+    
+    for (NSString *pair in pairs) {
+        NSArray *elements = [pair componentsSeparatedByString:@"="];
+        NSString *key = [[elements objectAtIndex:0] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        NSString *val = [[elements objectAtIndex:1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        
+        [dict setObject:val forKey:key];
+    }
+    return dict;
+}
+
+- (IBAction)getTest:(id)sender {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"myappscheme://test_page/one?token=12345&domain=foo.com"]];
+}
+
+
 @end
+
+
